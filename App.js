9be742d41bf1f61578d4f3a614a4da3e01e0aff9
git-demo2 <vaibhavahir4456@gmail.com>
2024-07-02 16:00:41 +0530

    add video effact

--- App.js
+++ App.js
@@ -56,3 +56,29 @@ function tastymonials() {
   });
 }
 document.addEventListener("DOMContentLoaded", tastymonials);
+
+
+function videoanimation() {
+  var videocon = document.querySelector(".video-contanier");
+  var playbtn = document.querySelector(".play");
+
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  videocon.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 40,
+      top: dets.y - 80,
+    });
+  });
+}
+videoanimation();
