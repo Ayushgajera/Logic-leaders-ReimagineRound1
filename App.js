4d9dc9798898c53a3f22c4919da2a872481d3eac
RITU SINGH <rs6166997@gmail.com>
2024-07-04 03:27:03 +0530

    main slider

--- App.js
+++ App.js
@@ -6,3 +6,11 @@ tl.from(h1, {
   y: -100,
   ease: "power2.out",
 });
+
+function showSiderbar(){
+  const sidebar = document.querySelector('.menu_slider')
+  sidebar.style.display = "flex"
+}
+function hidesidbar(){
+  const sidebar = document.querySelector('.menu_slider')
+  sidebar.style.display = "none"
