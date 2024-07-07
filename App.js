c550043a9dcf4109801cf94d8894f518818f49fd
Ayush Gajera <ayushgajera50@gmail.com>
2024-07-07 21:19:54 +0530

    project done

--- App.js
+++ App.js
@@ -1,109 +1,13 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
-const sections = gsap.utils.toArray(".container > section");
-let scrollTween = gsap.to(sections, {
-  xPercent: -95 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: 1,
-    end: "+=3000",
-  },
-});
-
-
-sections.forEach((section) => {
-  gsap.from(section.querySelector("img"), {
-    y: -150,
-    opacity: 0,
-    duration: 2,
-    ease: "elastic",
-    stagger: 1,
-    scrollTrigger: {
-      trigger: section,
-      containerAnimation: scrollTween,
-      start: "left right",
-      scrub:4,
-    },
-  });
-});
-
-
-// Define the tastymonials function
-function tastymonials (){
-
-  gsap.registerPlugin(ScrollTrigger);
-
-  document.addEventListener("DOMContentLoaded", function() {
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach((card, index) => {
-      if (index < cards.length - 1) {
-        const nextCard = cards[index + 1];
-        const toScale = 1 - (cards.length - 1 - index) * 0.1;
-        const toBrightness = 0.2;
-
-        ScrollTrigger.create({
-          trigger: nextCard,
-          start: "top bottom",
-          end: "top top",
-          onUpdate: (self) => {
-            const progress = self.progress;
-            card.style.transform = `scale(${1 - progress * (1 - toScale)})`;
-            card.style.filter = `brightness(${1 - progress * (1 - toBrightness)})`;
-          }
-        });
-      }
-    });
-  });
-}
-
-
-// ---------color change----------
-
-function colorChange(){
-  
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: '.firstpage',
-      // markers: true,
-      start: '80% 0%',
-      end: '90%  0%',
-      scrub: 1,
-    },
-  });
-  
-  tl.to(".bestSellingProduct", {
-    backgroundColor: "#f2c49b",
-    duration: 1
-  });
-  tl.to(".video-contanier", {
-    backgroundColor: "#f2c49b",
-    duration: 1
-  });
-}
-
-
-function sheryjs (){
-  
-  Shery.imageEffect(".top", {
-    style: 5, //Select Style
-    config:{"a":{"value":2,"range":[0,30]},"b":{"value":0.86,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.2427184466019416},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":1.53,"range":[1,15]},"durationOut":{"value":1.07,"range":[0.1,5]},"durationIn":{"value":1.04,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.06,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":1},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2],"_gsap":{"id":3}},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
-    // debug: true, // Debug Panel
-    gooey: true, 
-    
-  });
-}
-
 function video() {
   gsap.from(".video", {
     duration: 1,
     opacity: 0,
     // y: 50,
-    scale:0.6,
-    start:"0% 100%",
+    scale:0.5,
+    start:"-80% -80%",
     ease: "expo",
     end:"bottom bottom",
     scrollTrigger: {
@@ -114,8 +18,38 @@ function video() {
     },
   });
 }
+video();
 
+function horizontal() {
+  const sections = gsap.utils.toArray(".container > section");
+  let scrollTween = gsap.to(sections, {
+    xPercent: -95 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".container",
+      pin: true,
+      scrub: 1,
+      end: "+=3000",
+    },
+  });
 
+  sections.forEach((section) => {
+    gsap.from(section.querySelector("img"), {
+      y: -150,
+      opacity: 0,
+      duration: 2,
+      ease: "elastic",
+      stagger: 1,
+      scrollTrigger: {
+        trigger: section,
+        containerAnimation: scrollTween,
+        start: "left right",
+        scrub: 4,
+      },
+    });
+  });
+}
+horizontal();
 
 function mainpage() {
   let tl = gsap.timeline();
@@ -191,8 +125,84 @@ function menuani() {
   });
 }
 menuani();
+
+
+function colorChange(){
+  
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.firstpage',
+      // markers: true,
+      start: '80% 0%',
+      end: '90%  0%',
+      scrub: 1,
+    },
+  });
+  
+  tl.to(".bestSellingProduct", {
+    backgroundColor: "#f2c49b",
+    duration: 1
+  });
+  tl.to(".video-contanier", {
+    backgroundColor: "#f2c49b",
+    duration: 1
+  });
+}
+
+
+colorChange()
+
+const images = ['./images/main-img.png', './images/home-2.png', ];
+
+// Get the image element
+const slideshow = document.getElementById('slideshow');
+
+// Initial image index
+let currentIndex = 0;
+
+// Function to change the image
+function changeImage() {
+    // Update the image source
+    slideshow.src = images[currentIndex];
+
+    // Update the index to the next image, looping back to the start if necessary
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(changeImage, 2500);
+
+
+// Define the tastymonials function
+function tastymonials (){
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const cards = document.querySelectorAll('.card');
+
+    cards.forEach((card, index) => {
+      if (index < cards.length - 1) {
+        const nextCard = cards[index + 1];
+        const toScale = 1 - (cards.length - 1 - index) * 0.1;
+        const toBrightness = 0.2;
+
+        ScrollTrigger.create({
+          trigger: nextCard,
+          start: "top bottom",
+          end: "top top",
+          onUpdate: (self) => {
+            const progress = self.progress;
+            card.style.transform = `scale(${1 - progress * (1 - toScale)})`;
+            card.style.filter = `brightness(${1 - progress * (1 - toBrightness)})`;
+          }
+        });
+      }
+    });
+  });
+}
+
+
 tastymonials();
-colorChange();  
-sheryjs();
-video();
+
+
+
 
